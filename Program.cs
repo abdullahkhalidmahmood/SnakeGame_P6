a69b592fbfd413351a1188332ca41c042ad329c1
sharonne96 <4331826@students.swinburne.edu.my>
2020-04-22 13:17:08 +0800

    add functions for sound effects, draw elements and directions

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Collections;
 using System.Threading;
 using System.Media;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using System.Data;
 
 namespace Snake
 {
@@ -22,11 +25,83 @@ namespace Snake
 
     class Program
     {
+        /// <summary>
+        /// Functions start here
+        /// </summary>
+        public void BackgroundMusic()
+        {
+            //Create SoundPlayer objbect to control sound playback
+            SoundPlayer backgroundMusic = new SoundPlayer();
+            
+            //Locate the SoundPlayer to the correct sound directory
+            backgroundMusic.SoundLocation = AppDomain.CurrentDomain.BaseDirectory + "/SnakeBGM_1_Extended.wav";
+            
+            //Play the background music at the beginning
+            backgroundMusic.Play();
+        }
+
+        public void SoundEffect()
+        {
+            SoundPlayer playerDie = new SoundPlayer();
+            playerDie.SoundLocation = AppDomain.CurrentDomain.BaseDirectory + "/SnakeDie_1.wav";
+            playerDie.Play(); //Play the die sound effect after player died
+        }
+
+        public void DrawFood()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("@");
+        }
+        
+        public void DrawObstacle()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("=");
+        }
+
+        public void DrawSnakeBody()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("*");
+        }
+        
+        public void Direction(Position[] directions)
+        {
+            
+            directions[0] = new Position(0, 1);
+            directions[1] = new Position(0, -1);
+            directions[2] = new Position(1, 0);
+            directions[3] = new Position(-1, 0);
+
+        }
+       public void InitialRandomObstacles(List<Position>obstacles)
+        {
+            //Create obstacles objects and initialise certain random position of obstacles at every game play
+            //The randomise obstacles will not exist in the first row at the beginning.
+            Random randomNumbersGenerator = new Random();
+            obstacles.Add(new Position(randomNumbersGenerator.Next(1, Console.WindowHeight), randomNumbersGenerator.Next(0, Console.WindowWidth)));
+            obstacles.Add(new Position(randomNumbersGenerator.Next(1, Console.WindowHeight), randomNumbersGenerator.Next(0, Console.WindowWidth)));
+            obstacles.Add(new Position(randomNumbersGenerator.Next(1, Console.WindowHeight), randomNumbersGenerator.Next(0, Console.WindowWidth)));
+            obstacles.Add(new Position(randomNumbersGenerator.Next(1, Console.WindowHeight), randomNumbersGenerator.Next(0, Console.WindowWidth)));
+            obstacles.Add(new Position(randomNumbersGenerator.Next(1, Console.WindowHeight), randomNumbersGenerator.Next(0, Console.WindowWidth)));
+        }
+
+        
+        /// <summary>
+        /// Funstions end here
+        /// </summary>
+
+
+
+        /// <summary>
+        /// Main starts here
+        /// </summary>
         //Define direction by using index number
         //Set the time taken for the food to be dissappear
         //Initialise negative points
         static void Main(string[] args)
         {
+            
             byte right = 0;
             byte left = 1;
             byte down = 2;
@@ -34,27 +109,17 @@ namespace Snake
             int lastFoodTime = 0;
             int foodDissapearTime = 8000;
             int negativePoints = 0;
+            Position[] directions = new Position[4];
+
+            Program p = new Program();
+            p.BackgroundMusic();
 
             // Define direction with characteristic of index of array
-            Position[] directions = new Position[]
-            {
-                new Position(0, 1), // right
-                new Position(0, -1), // left
-                new Position(1, 0), // down
-                new Position(-1, 0), // up
-            };
-            
-            
-            //Create SoundPlayer objbect to control sound playback
-            SoundPlayer playerbgm = new SoundPlayer();
-            SoundPlayer playerdie = new SoundPlayer();
+            p.Direction(directions);
 
-            //Locate the SoundPlayer to the correct sound directory
-            playerbgm.SoundLocation = AppDomain.CurrentDomain.BaseDirectory + "/SnakeBGM_1_Extended.wav";
-            playerdie.SoundLocation = AppDomain.CurrentDomain.BaseDirectory + "/SnakeDie_1.wav";
+            List<Position> obstacles = new List<Position>();
+            p.InitialRandomObstacles(obstacles);
 
-            //Play the background music at the beginning
-            playerbgm.Play();
 
             //Do the initialization for sleepTime (Game's Speed), Snake's direction and food timing
             //Limit the number of rows of text accessible in the console window
@@ -64,23 +129,13 @@ namespace Snake
             Console.BufferHeight = Console.WindowHeight;
             lastFoodTime = Environment.TickCount;
 
-            //Create obstacles objects and initialise certain random position of obstacles at every game play
-            //The randomise obstacles will not exist in the first row at the beginning.
-            List<Position> obstacles = new List<Position>()
-            {
-                new Position(randomNumbersGenerator.Next(1, Console.WindowHeight), randomNumbersGenerator.Next(0, Console.WindowWidth)),
-                new Position(randomNumbersGenerator.Next(1, Console.WindowHeight), randomNumbersGenerator.Next(0, Console.WindowWidth)),
-                new Position(randomNumbersGenerator.Next(1, Console.WindowHeight), randomNumbersGenerator.Next(0, Console.WindowWidth)),
-                new Position(randomNumbersGenerator.Next(1, Console.WindowHeight), randomNumbersGenerator.Next(0, Console.WindowWidth)),
-                new Position(randomNumbersGenerator.Next(1, Console.WindowHeight), randomNumbersGenerator.Next(0, Console.WindowWidth)),
-            };
+            
 
             //Show the obstacle in the windows with marking of "="
             foreach (Position obstacle in obstacles)
             {
-                Console.ForegroundColor = ConsoleColor.Cyan;
                 Console.SetCursorPosition(obstacle.col, obstacle.row);
-                Console.Write("=");
+                p.DrawObstacle();
             }
 
             //Initialise the snake position in top left corner of the windows
@@ -90,35 +145,33 @@ namespace Snake
             {
                 snakeElements.Enqueue(new Position(0, i));
             }
-            
+
             //To position food randomly when the program runs first time
-            Position food;
+            Position food = new Position();
             do
-            {   
+            {
                 food = new Position(randomNumbersGenerator.Next(0, Console.WindowHeight), //Food generated within console height
                     randomNumbersGenerator.Next(0, Console.WindowWidth)); //Food generate within console width
             }
             //a loop is created - while the program contains food and the obstacle is not hit 
             //put food on different position which is "@"
-            while (snakeElements.Contains(food) || obstacles.Contains(food)); 
+            while (snakeElements.Contains(food) || obstacles.Contains(food));
             Console.SetCursorPosition(food.col, food.row);
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Write("@");
-            
+            p.DrawFood();
+
             //while the game is running position snake on terminal with shape "*"
             foreach (Position position in snakeElements)
             {
                 Console.SetCursorPosition(position.col, position.row);
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-                Console.Write("*");
+                p.DrawSnakeBody();
             }
-            
+
             while (true)
             {
                 //negative points is initialized as 0 at the beginning of the game. As the player reaches out for food
                 //negative points increment depending how far the food is
                 negativePoints++;
-                
+
                 //User key pressed statement: depends on which direction the user want to go to get food or avoid obstacle
                 if (Console.KeyAvailable)
                 {
@@ -143,11 +196,11 @@ namespace Snake
                 //When the game starts the snake head is towards the end of his body with face direct to start from right.
                 Position snakeHead = snakeElements.Last();
                 Position nextDirection = directions[direction];
-                
+
                 //Snake position to go within the terminal window assigned.
                 Position snakeNewHead = new Position(snakeHead.row + nextDirection.row,
                     snakeHead.col + nextDirection.col);
-                
+
                 if (snakeNewHead.col < 0) snakeNewHead.col = Console.WindowWidth - 1;
                 if (snakeNewHead.row < 0) snakeNewHead.row = Console.WindowHeight - 1;
                 if (snakeNewHead.row >= Console.WindowHeight) snakeNewHead.row = 0;
@@ -156,7 +209,7 @@ namespace Snake
                 //If snake head hits the obstacle the game is over and the player will start a new game
                 if (snakeElements.Contains(snakeNewHead) || obstacles.Contains(snakeNewHead))
                 {
-                    playerdie.Play(); //Play the die sound effect after player died
+                    p.SoundEffect();
                     Console.SetCursorPosition(0, 0);
                     Console.ForegroundColor = ConsoleColor.Red;//Text color for game over
                     Console.WriteLine("Game over!");//The text which user will view when game is over
@@ -169,17 +222,17 @@ namespace Snake
                 }
                 //The way snake head will change as the player changes his direction
                 Console.SetCursorPosition(snakeHead.col, snakeHead.row);
-                Console.ForegroundColor = ConsoleColor.DarkGray;//snake body color
-                Console.Write("*");//snake body style
+                p.DrawSnakeBody();
 
                 //Snake head shape when the user presses the key to change his direction
                 snakeElements.Enqueue(snakeNewHead);
                 Console.SetCursorPosition(snakeNewHead.col, snakeNewHead.row);
                 Console.ForegroundColor = ConsoleColor.Gray;
-                if (direction == right) Console.Write(">");//Snake head when going right
+                if (direction == right) Console.Write(">"); //Snake head when going right
                 if (direction == left) Console.Write("<");//Snake head when going left
                 if (direction == up) Console.Write("^");//Snake head when going up
                 if (direction == down) Console.Write("v");//Snake head when going down
+
 
                 // food will be positioned randomly until they are not at the same row & column as snake head
                 if (snakeNewHead.col == food.col && snakeNewHead.row == food.row)
@@ -190,16 +243,15 @@ namespace Snake
                         food = new Position(randomNumbersGenerator.Next(0, Console.WindowHeight),
                             randomNumbersGenerator.Next(0, Console.WindowWidth));
                     }
-                    
+
                     //when the snake eat the food, the system tickcount will be set as lastFoodTime
                     //new food will be drawn, snake speed will increases
                     while (snakeElements.Contains(food) || obstacles.Contains(food));
                     lastFoodTime = Environment.TickCount;
                     Console.SetCursorPosition(food.col, food.row);
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write("@");
+                    p.DrawFood();
                     sleepTime--;
-                    
+
                     //setting position of new obstacles randomly
                     Position obstacle = new Position();
                     do
@@ -214,8 +266,7 @@ namespace Snake
                         (food.row != obstacle.row && food.col != obstacle.row));
                     obstacles.Add(obstacle);
                     Console.SetCursorPosition(obstacle.col, obstacle.row);
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.Write("=");
+                    p.DrawObstacle();
                 }
                 else
                 {
@@ -224,7 +275,7 @@ namespace Snake
                     Console.SetCursorPosition(last.col, last.row);
                     Console.Write(" ");
                 }
-                   
+
                 //if snake did not eat the food before it disappears, 50 will be added to negative points
                 //draw new food after the previous one disappeared
                 if (Environment.TickCount - lastFoodTime >= foodDissapearTime)
@@ -232,7 +283,7 @@ namespace Snake
                     negativePoints = negativePoints + 50;
                     Console.SetCursorPosition(food.col, food.row);
                     Console.Write(" ");
-                    
+
                     do
                     {
                         food = new Position(randomNumbersGenerator.Next(0, Console.WindowHeight),
@@ -243,12 +294,11 @@ namespace Snake
                 }
                 //draw food
                 Console.SetCursorPosition(food.col, food.row);
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write("@");
-                
+                p.DrawFood();
+
                 //snake moving speed increased 
                 sleepTime -= 0.01;
-                
+
                 //pause the execution thread of snake moving speed
                 Thread.Sleep((int)sleepTime);
             }
