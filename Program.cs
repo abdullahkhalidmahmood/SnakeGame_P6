a2e92ec031fe31b967299a46fc568e49b30631c5
yeatnaicheng <100078269@students.swinburne.edu.my>
2020-05-11 15:40:59 +0800

    Add the lives to the snake and change the win score calculation

--- Program.cs
+++ Program.cs
@@ -187,25 +187,29 @@ namespace Snake
         /// <param name="negativePoints"></param>
         /// <param name="obstacles"></param>
         /// <returns></returns>
-        public int GameOverCheck(int currentTime, Queue<Position> snakeElements, Position snakeNewHead,int negativePoints, List<Position> obstacles)
+        public int GameOverCheck(int currentTime, Queue<Position> snakeElements, Position snakeNewHead,int negativePoints, List<Position> obstacles,ref int userPoints,ref int finalScore, ref int life)
         {
             if (snakeElements.Contains(snakeNewHead) || obstacles.Contains(snakeNewHead) || (Environment.TickCount-currentTime) > 30000
                 || snakeNewHead.row >= Console.WindowHeight || snakeNewHead.col >= Console.WindowWidth || snakeNewHead.row < 2 || snakeNewHead.col < 0)
             {
-                LoseSoundEffect(); //this sound effect will be play if game over
-                Console.SetCursorPosition(0, 0);
-                Console.ForegroundColor = ConsoleColor.Red;//Text color for game over
-               
-                int userPoints = (snakeElements.Count - 4) * 100 - negativePoints;//points calculated for player
-                userPoints = Math.Max(userPoints, 0); //if (userPoints < 0) userPoints = 0;
-                
-                //Display game over text and points
-                PrintLinesInCenter("Game Over!", "Your points are:" + userPoints, "Press enter to exit the game!");
-                
-                SavePointsToFile(userPoints);//saving points to files
+                life--;
+                //The player will get game over if his life becomes 0
+                if (life == 0)
+                {
+                    LoseSoundEffect(); //this sound effect will be play if game over
+                    Console.SetCursorPosition(0, 0);
+                    Console.ForegroundColor = ConsoleColor.Red;//Text color for game over
 
-                //close only when enter key is pressed
-                while (Console.ReadKey().Key != ConsoleKey.Enter) {}
+                    //Display game over text and points
+                    PrintLinesInCenter("Game Over!", "Your points are:" + finalScore, "Press enter to exit the game!");
+
+                    SavePointsToFile(finalScore);//saving points to files
+
+                    //close only when enter key is pressed
+                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                    return 1;
+                }
+                finalScore += userPoints;
                 return 1;
             }
             return 0;
@@ -217,7 +221,7 @@ namespace Snake
         /// <param name="snakeElements"></param>
         /// <param name="negativePoints"></param>
         /// <returns></returns>
-        public int WinningCheck(Queue<Position> snakeElements, int negativePoints)
+        public int WinningCheck(Queue<Position> snakeElements, int negativePoints, ref int userPoints, ref int finalScore, int life)
         {
             // initially snake elements has 4, increment 1 by eating 1 food, so eat 3 food to get 7 snake elements 
             if (snakeElements.Count == 7)
@@ -226,12 +230,11 @@ namespace Snake
                 Console.SetCursorPosition(0, 0);
                 Console.ForegroundColor = ConsoleColor.Green;//Text color for game won
 
-                int userPoints = (snakeElements.Count - 4) * 100 - negativePoints;//points calculated for player
-                userPoints = Math.Max(userPoints, 0); //if (userPoints < 0) userPoints = 0;
+                finalScore = finalScore + userPoints +life*1000;
 
                 //display game won text and user points
-                PrintLinesInCenter("You Win!", "Your points are:" + userPoints, "Press enter to exit the game!");
-                SavePointsToFile(userPoints);//saving points to files
+                PrintLinesInCenter("You Win!","Life Point Left:"+life,"Bonus Score: +"+ life*1000, "Your points are:" + finalScore, "Press enter to exit the game!");
+                SavePointsToFile(finalScore);//saving points to files
 
                 //close only when enter key is pressed
                 while (Console.ReadKey().Key != ConsoleKey.Enter) { }
@@ -387,14 +390,14 @@ namespace Snake
         /// <param name="userPoints"></param>
         /// <param name="snakeElements"></param>
         /// <param name="negativePoints"></param>
-        public void PrintUserPoint(int userPoints, Queue<Position> snakeElements, int negativePoints)
+        public void PrintUserPoint(ref int userPoints, Queue<Position> snakeElements, int negativePoints)
         {
             userPoints = (snakeElements.Count - 4) * 100 - negativePoints;//points calculated for player
             userPoints = Math.Max(userPoints, 0); //if (userPoints < 0) userPoints = 0;
             Console.SetCursorPosition(40, 0);
             Console.WriteLine("                  ");
             Console.SetCursorPosition(40, 0);
-            Console.WriteLine("Score: {0}", userPoints);
+            Console.WriteLine("Point: {0}", userPoints);
         }
 
 
@@ -414,8 +417,9 @@ namespace Snake
             int currentTime = Environment.TickCount;
             int lastFoodTime = 0;
             int foodDissapearTime = 10000; //food dissappears after 10 second 
-            int negativePoints = 0;
+            int life = 3;
             int userPoints = 0;
+            int finalScore = 0;
             Console.SetWindowSize(100, 38);//reducing screen size 
             Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
             
@@ -427,141 +431,151 @@ namespace Snake
             //disbale the resize of console window by disabling maximize button and border dragging
             DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), maximizeButton, console);
             DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), consoleBorder, console);
-
        
             //display start screen before background music and game start 
             p.DisplayStartScreen();
-            //Play background music
-            p.BackgroundMusic();
-
+          
             // Define direction with characteristic of index of array
             p.Direction(directions);
 
-            // Initialised the obstacles location at the starting of the game
-            List<Position> obstacles = new List<Position>();
-            p.InitialRandomObstacles(obstacles);
-
-            //Do the initialization for sleepTime (Game's Speed), Snake's direction and food timing
-            //Limit the number of rows of text accessible in the console window
-            double sleepTime = 100;
-            int direction = right;
-            Random randomNumbersGenerator = new Random();
-            Console.BufferHeight = Console.WindowHeight;
-            lastFoodTime = Environment.TickCount;
-
-            //Initialise the snake position in 3rd row of top left corner of the windows
-            //Havent draw the snake elements in the windows yet. Will be drawn in the code below
-            Queue<Position> snakeElements = new Queue<Position>();
-            for (int i = 0; i <= 3; i++) // Length of the snake was reduced to 3 units of *
+            while (life > 0)
             {
-                snakeElements.Enqueue(new Position(2, i));
-            }
+                //Play background music
+                p.BackgroundMusic();
+                int negativePoints = 0;
+                //Clear the console everytime start the game in new life
+                Console.Clear();
+                
+                // Initialised the obstacles location at the starting of the game
+                List<Position> obstacles = new List<Position>();
+                p.InitialRandomObstacles(obstacles);
 
-            //To position food randomly when the program runs first time
-            Position food = new Position();
-            p.GenerateFood(ref food,snakeElements,obstacles);
-            
-            //while the game is running position snake on terminal with shape "*"
-            foreach (Position position in snakeElements)
-            {
-                Console.SetCursorPosition(position.col, position.row);
-                p.DrawSnakeBody();
-            }
+                //Do the initialization for sleepTime (Game's Speed), Snake's direction and food timing
+                //Limit the number of rows of text accessible in the console window
+                double sleepTime = 100;
+                int direction = right;
+                Random randomNumbersGenerator = new Random();
+                Console.BufferHeight = Console.WindowHeight;
+                lastFoodTime = Environment.TickCount;
 
-            while (true)
-            {
-                //Print the food count down timer
-                p.PrintFoodCountDownTimer(lastFoodTime);
-
-                //negative points is initialized as 0 at the beginning of the game. As the player reaches out for food
-                //negative points increment depending how far the food is
-                negativePoints++;
-
-                //Check the user input direction
-                p.CheckUserInput(ref direction, right, left, down, up);
-              
-                //When the game starts the snake head is towards the end of his body with face direct to start from right.
-                Position snakeHead = snakeElements.Last();
-                Position nextDirection = directions[direction];
-
-                //Snake position to go within the terminal window assigned.
-                Position snakeNewHead = new Position(snakeHead.row + nextDirection.row,
-                    snakeHead.col + nextDirection.col);
-
-/*                if (snakeNewHead.col < 0) snakeNewHead.col = Console.WindowWidth - 1;
-                if (snakeNewHead.row < 0) snakeNewHead.row = Console.WindowHeight - 1;
-                if (snakeNewHead.row >= Console.WindowHeight) snakeNewHead.row = 0;
-                if (snakeNewHead.col >= Console.WindowWidth) snakeNewHead.col = 0;*/
-
-                //print realtime user points
-                p.PrintUserPoint(userPoints, snakeElements, negativePoints);
-
-                //Check for GameOver Criteria
-                int gameOver=p.GameOverCheck(currentTime, snakeElements, snakeNewHead, negativePoints,obstacles);
-                if (gameOver == 1)
-                    return;
-
-                //Check for Winning Criteria
-                int winning = p.WinningCheck(snakeElements, negativePoints);
-                if (winning == 1) return;
-
-                //The way snake head will change as the player changes his direction
-                Console.SetCursorPosition(snakeHead.col, snakeHead.row);
-                p.DrawSnakeBody();
-
-                //Snake head shape when the user presses the key to change his direction
-                snakeElements.Enqueue(snakeNewHead);
-                Console.SetCursorPosition(snakeNewHead.col, snakeNewHead.row);
-                Console.ForegroundColor = ConsoleColor.Gray;
-                if (direction == right) Console.Write(">"); //Snake head when going right
-                if (direction == left) Console.Write("<");//Snake head when going left
-                if (direction == up) Console.Write("^");//Snake head when going up
-                if (direction == down) Console.Write("v");//Snake head when going down
-
-
-                // food will be positioned randomly until they are not at the same row & column as snake head
-                if (snakeNewHead.col == food.col && snakeNewHead.row == food.row)
+                //Initialise the snake position in 3rd row of top left corner of the windows
+                //Havent draw the snake elements in the windows yet. Will be drawn in the code below
+                Queue<Position> snakeElements = new Queue<Position>();
+                for (int i = 0; i <= 3; i++) // Length of the snake was reduced to 3 units of *
                 {
-                    Console.Beep();// Make a sound effect when food was eaten.
-                    p.GenerateFood(ref food, snakeElements, obstacles);
-                    
-
-                    //when the snake eat the food, the system tickcount will be set as lastFoodTime
-                    //new food will be drawn, snake speed will increases
-                    lastFoodTime = Environment.TickCount;
-                    sleepTime--;
-
-                    //Generate new obstacle
-                    p.GenerateNewObstacle(ref food,snakeElements,obstacles);
-                    
-                }
-                else
-                {
-                    // snake is moving
-                    Position last = snakeElements.Dequeue();
-                    Console.SetCursorPosition(last.col, last.row);
-                    Console.Write(" ");
+                    snakeElements.Enqueue(new Position(2, i));
                 }
 
-                //if snake did not eat the food before it disappears, 50 will be added to negative points
-                //draw new food after the previous one disappeared
-                if (Environment.TickCount - lastFoodTime >= foodDissapearTime)
+                //To position food randomly when the program runs first time
+                Position food = new Position();
+                p.GenerateFood(ref food, snakeElements, obstacles);
+
+                //while the game is running position snake on terminal with shape "*"
+                foreach (Position position in snakeElements)
                 {
-                    negativePoints = negativePoints + 50;
-                    Console.SetCursorPosition(food.col, food.row);
-                    Console.Write(" ");
-
-                    //Generate the new food and record the system tick count
-                    p.GenerateFood(ref food, snakeElements, obstacles);
-                    lastFoodTime = Environment.TickCount;
+                    Console.SetCursorPosition(position.col, position.row);
+                    p.DrawSnakeBody();
                 }
-             
-                //snake moving speed increased 
-                sleepTime -= 0.01;
 
-                //pause the execution thread of snake moving speed
-                Thread.Sleep((int)sleepTime);
+                while (true)
+                {
+                    //Print the food count down timer
+                    p.PrintFoodCountDownTimer(lastFoodTime);
+
+                    //negative points is initialized as 0 at the beginning of the game. As the player reaches out for food
+                    //negative points increment depending how far the food is
+                    negativePoints++;
+
+                    //Check the user input direction
+                    p.CheckUserInput(ref direction, right, left, down, up);
+
+                    //When the game starts the snake head is towards the end of his body with face direct to start from right.
+                    Position snakeHead = snakeElements.Last();
+                    Position nextDirection = directions[direction];
+
+                    //Snake position to go within the terminal window assigned.
+                    Position snakeNewHead = new Position(snakeHead.row + nextDirection.row,
+                        snakeHead.col + nextDirection.col);
+
+                    /*                if (snakeNewHead.col < 0) snakeNewHead.col = Console.WindowWidth - 1;
+                                    if (snakeNewHead.row < 0) snakeNewHead.row = Console.WindowHeight - 1;
+                                    if (snakeNewHead.row >= Console.WindowHeight) snakeNewHead.row = 0;
+                                    if (snakeNewHead.col >= Console.WindowWidth) snakeNewHead.col = 0;*/
+
+                    //print realtime user points
+                    p.PrintUserPoint(ref userPoints, snakeElements, negativePoints);
+
+                    //Check for GameOver Criteria including the Final Game Over and Die Condition
+                    int gameOver = p.GameOverCheck(currentTime, snakeElements, snakeNewHead, negativePoints, obstacles,ref userPoints,ref finalScore, ref life);
+                    if (gameOver == 1)
+                        break;
+        
+
+                    //Check for Winning Criteria
+                    int winning = p.WinningCheck(snakeElements, negativePoints,ref userPoints,ref finalScore ,life);
+                    if (winning == 1)
+                        return;
+
+                    //The way snake head will change as the player changes his direction
+                    Console.SetCursorPosition(snakeHead.col, snakeHead.row);
+                    p.DrawSnakeBody();
+
+                    //Snake head shape when the user presses the key to change his direction
+                    snakeElements.Enqueue(snakeNewHead);
+                    Console.SetCursorPosition(snakeNewHead.col, snakeNewHead.row);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    if (direction == right) Console.Write(">"); //Snake head when going right
+                    if (direction == left) Console.Write("<");//Snake head when going left
+                    if (direction == up) Console.Write("^");//Snake head when going up
+                    if (direction == down) Console.Write("v");//Snake head when going down
+
+
+                    // food will be positioned randomly until they are not at the same row & column as snake head
+                    if (snakeNewHead.col == food.col && snakeNewHead.row == food.row)
+                    {
+                        Console.Beep();// Make a sound effect when food was eaten.
+                        p.GenerateFood(ref food, snakeElements, obstacles);
+
+
+                        //when the snake eat the food, the system tickcount will be set as lastFoodTime
+                        //new food will be drawn, snake speed will increases
+                        lastFoodTime = Environment.TickCount;
+                        sleepTime--;
+
+                        //Generate new obstacle
+                        p.GenerateNewObstacle(ref food, snakeElements, obstacles);
+
+                    }
+                    else
+                    {
+                        // snake is moving
+                        Position last = snakeElements.Dequeue();
+                        Console.SetCursorPosition(last.col, last.row);
+                        Console.Write(" ");
+                    }
+
+                    //if snake did not eat the food before it disappears, 50 will be added to negative points
+                    //draw new food after the previous one disappeared
+                    if (Environment.TickCount - lastFoodTime >= foodDissapearTime)
+                    {
+                        negativePoints = negativePoints + 50;
+                        Console.SetCursorPosition(food.col, food.row);
+                        Console.Write(" ");
+
+                        //Generate the new food and record the system tick count
+                        p.GenerateFood(ref food, snakeElements, obstacles);
+                        lastFoodTime = Environment.TickCount;
+                    }
+
+                    //snake moving speed increased 
+                    sleepTime -= 0.01;
+
+                    //pause the execution thread of snake moving speed
+                    Thread.Sleep((int)sleepTime);
+                }
             }
+
+            return;
         }
     }
 }
